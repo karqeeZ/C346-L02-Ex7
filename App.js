6242251e7bfc90047f3ec91349a5de8bb796fc5c
Johnson <23009886@myrp.edu.sg>
2024-10-24 00:33:01 +0800

    Initial

--- App.js
+++ App.js
@@ -7,13 +7,12 @@ const App = () => {
       <ScrollView>
           <StatusBar barStyle="light-content" />
           <Text>GetABoat - For Sale</Text>
-          <Text>  </Text>
-          <Boat name=" Sea Ray 500 Sundancer" description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away." picture={require("./img/sea_ray.jpg")}/>
-          <Boat name=" Four Winns Horizon 180" description="A sporty look and refined details truly set the Horizon 180 above all others." picture={require("./img/four_winns.jpg")}/>
-          <Boat name=" Flipper 640 ST" description="A modern take on the classic, traditional hardtop and perfect for a family picnic." picture={require("./img/flipper.jpg")}/>
-          <Boat name=" Princess V48" description="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior." picture={require("./img/princess.jpg")}/>
-          <Boat name=" Bayliner 175 Bowrider" description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance." picture={require("./img/bayliner.jpg")}/>
-          <Boat name=" Fairline Targa 47" description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit." picture={require("./img/fairline.jpg")}/>
+          <Boat name=" Sea Ray 500 Sundancer" icon_name="search1" description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away." picture={require("./img/sea_ray.jpg")}/>
+          <Boat name=" Four Winns Horizon 180" icon_name="search1" description="A sporty look and refined details truly set the Horizon 180 above all others." picture={require("./img/four_winns.jpg")}/>
+          <Boat name=" Flipper 640 ST" icon_name="search1" description="A modern take on the classic, traditional hardtop and perfect for a family picnic." picture={require("./img/flipper.jpg")}/>
+          <Boat name=" Princess V48" icon_name="search1" description="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior." picture={require("./img/princess.jpg")}/>
+          <Boat name=" Bayliner 175 Bowrider" icon_name="search1" description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance." picture={require("./img/bayliner.jpg")}/>
+          <Boat name=" Fairline Targa 47" icon_name="search1" description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit." picture={require("./img/fairline.jpg")}/>
       </ScrollView>
   )
 };
--- component/Boat.js
+++ component/Boat.js
@@ -1,12 +1,37 @@
 import React from "react";
-import {Image, Text, View} from "react-native";
+import {Image, Text, View, StyleSheet} from "react-native";
+import Icon from 'react-native-vector-icons/AntDesign';
 
-const Boat = ({name,description,picture}) => {
+const Boat = ({name,description,icon_name,picture}) => {
     return (
-        <View>
-            <Text>{name}</Text>
-            <Text>{description}</Text>
-            <Image source={picture} style={{width:400,height:350}}/>
+        <View style={styles.container}>
+            <Text style={styles.title}><Icon name={icon_name}/>{name}</Text>
+            <Text style={styles.description}>{description}</Text>
+            <Image source={picture} style={{width:400,height:400}}/>
+            <Text> </Text>
         </View>
     )
-};export default Boat;
+};
+
+
+const styles= StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: 'grey',
+        fontFamily: 'monospace'
+    },
+    description: {
+        fontSize: 15,
+        color: 'grey',
+        textAlign: 'center',
+    }
+});
+
+export default Boat;
