ea09fdec5479f501f1a14ebb9958ac463c4eef9f
Johnson <23009886@myrp.edu.sg>
2024-11-07 19:58:58 +0800

    styling update

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import {ScrollView, StatusBar, Text} from 'react-native';
+import { ScrollView, StatusBar, Text, StyleSheet } from 'react-native';
 import Boat from './component/Boat.js';
 
 const App = () => {
-  return (
-      <ScrollView>
-          <StatusBar barStyle="light-content" />
-          <Text>GetABoat - For Sale</Text>
-          <Boat name=" Sea Ray 500 Sundancer" icon_name="search1" description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away." picture={require("./img/sea_ray.jpg")}/>
-          <Boat name=" Four Winns Horizon 180" icon_name="search1" description="A sporty look and refined details truly set the Horizon 180 above all others." picture={require("./img/four_winns.jpg")}/>
-          <Boat name=" Flipper 640 ST" icon_name="search1" description="A modern take on the classic, traditional hardtop and perfect for a family picnic." picture={require("./img/flipper.jpg")}/>
-          <Boat name=" Princess V48" icon_name="search1" description="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior." picture={require("./img/princess.jpg")}/>
-          <Boat name=" Bayliner 175 Bowrider" icon_name="search1" description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance." picture={require("./img/bayliner.jpg")}/>
-          <Boat name=" Fairline Targa 47" icon_name="search1" description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit." picture={require("./img/fairline.jpg")}/>
-      </ScrollView>
-  )
+    return (
+        <ScrollView style={styles.container}>
+            <StatusBar barStyle="light-content" />
+            <Text style={styles.title}>GetABoat - For Sale</Text>
+            <Boat name="Sea Ray 500 Sundancer" icon_name="search1" description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away." picture={require("./img/sea_ray.jpg")} />
+            <Boat name="Four Winns Horizon 180" icon_name="search1" description="A sporty look and refined details truly set the Horizon 180 above all others." picture={require("./img/four_winns.jpg")} />
+            <Boat name="Flipper 640 ST" icon_name="search1" description="A modern take on the classic, traditional hardtop and perfect for a family picnic." picture={require("./img/flipper.jpg")} />
+            <Boat name="Princess V48" icon_name="search1" description="There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior." picture={require("./img/princess.jpg")} />
+            <Boat name="Bayliner 175 Bowrider" icon_name="search1" description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance." picture={require("./img/bayliner.jpg")} />
+            <Boat name="Fairline Targa 47" icon_name="search1" description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit." picture={require("./img/fairline.jpg")} />
+        </ScrollView>
+    );
 };
 
-export default App;
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        paddingVertical: 10,
+    },
+    title: {
+        textAlign: 'center',
+        fontFamily: 'monospace',
+        color: 'white',
+        backgroundColor: 'steelblue',
+        paddingVertical: 15,
+        marginHorizontal: 15,
+        borderRadius: 8,
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        fontSize: 24,
+    }
+});
 
+export default App;
--- component/Boat.js
+++ component/Boat.js
@@ -1,37 +1,56 @@
 import React from "react";
-import {Image, Text, View, StyleSheet} from "react-native";
+import { Image, Text, View, StyleSheet } from "react-native";
 import Icon from 'react-native-vector-icons/AntDesign';
 
-const Boat = ({name,description,icon_name,picture}) => {
+const Boat = ({ name, description, icon_name, picture }) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}><Icon name={icon_name}/>{name}</Text>
+            <Text style={styles.title}><Icon name={icon_name} size={20} color="white" /> {name}</Text>
             <Text style={styles.description}>{description}</Text>
-            <Image source={picture} style={{width:400,height:400}}/>
-            <Text> </Text>
+            <Image source={picture} style={styles.image} />
         </View>
-    )
+    );
 };
 
-
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        borderWidth: 0.3,
+        borderColor: 'black',
+        shadowColor: "black",
+        elevation: 5,
+        margin: 10,
+        padding: 20,
         alignItems: 'center',
-        paddingHorizontal: 10,
     },
     title: {
-        fontSize: 25,
+        fontSize: 20,
         fontWeight: 'bold',
-        color: 'grey',
-        fontFamily: 'monospace'
+        color: 'white',
+        fontFamily: 'monospace',
+        backgroundColor: 'cadetblue',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 8,
+        marginBottom: 10,
+        textAlign: 'center',
     },
     description: {
-        fontSize: 15,
-        color: 'grey',
+        fontSize: 16,
+        color: 'dimgray',
         textAlign: 'center',
-    }
+        paddingHorizontal: 10,
+        marginBottom: 15,
+    },
+    image: {
+        width: 300,
+        height: 300,
+        borderRadius: 10,
+        borderWidth: 5,
+        borderColor: 'steelblue',
+        marginTop: 10,
+    },
 });
 
 export default Boat;
